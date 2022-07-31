433f690679c59fd551f1c884e70bb685a0f17074
Alyssa Davidson <alyssa.davidson17@gmail.com>
2022-07-31 18:27:54 -0400

    add tweet server and scheduler

--- lib/elixir_tweet/application.ex
+++ lib/elixir_tweet/application.ex
@@ -10,7 +10,8 @@ defmodule ElixirTweet.Application do
     children = [
       # Starts a worker by calling: ElixirTweet.Worker.start_link(arg)
       # {ElixirTweet.Worker, arg}
-      ElixirTweet.scheduler()
+      ElixirTweet.TweetServer,
+      ElixirTweet.Scheduler
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- lib/tweet.exs
+++ lib/tweet.exs
@@ -1,11 +1,12 @@
 defmodule ElixirTweet.Tweet do
   def send(str) do
-    ExTwitter.configure(:process, [
+    ExTwitter.configure(:process,
       consumer_key: System.get_env("Elixir_tweet_twitter_consumer_key"),
-      consumer_secret:System.get_env("Elixir_tweet_twitter_consumer_secret"),
+      consumer_secret: System.get_env("Elixir_tweet_twitter_consumer_secret"),
       access_token: System.get_env("Elixir_tweet_twitter_access_token"),
       access_token_secret: System.get_env("Elixir_tweet_twitter_access_secret")
-    ])
+    )
+
     ExTwitter.update(str)
   end
 end
